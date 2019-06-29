c52ecbe654345891543e7cbb2f88c0cd94d0c30c
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2019-06-29 13:33:08 +0200

    Version bump for new location

--- src/wordpress-plugins-topic-highlighter.meta.js
+++ src/wordpress-plugins-topic-highlighter.meta.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         WordPress.org plugins and themes topic highlighter
 // @namespace    http://clorith.net/
-// @version      0.3.4
+// @version      0.4.0
 // @description  Add status highlights to topics for easy overviews.
 // @author       Clorith
 // @match        https://wordpress.org/support/*
 // @match        https://*.wordpress.org/support/*
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
 // @resource     configHtml https://raw.githubusercontent.com/Clorith/wporg-topic-highlighter/master/src/options.html
+// @updateURL    https://github.com/wporg-support/topic-highlighter/raw/master/src/wordpress-plugins-topic-highlighter.user.js
+// @downloadURL  https://github.com/wporg-support/topic-highlighter/raw/master/src/wordpress-plugins-topic-highlighter.user.js
 // @grant        GM_getResourceText
 // ==/UserScript==
